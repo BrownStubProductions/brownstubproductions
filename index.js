dbed5a35ee79ec52d16086c1c4df7f4532f05ce4
BrownStubProductions <64936058+BrownStubProductions@users.noreply.github.com>
2020-06-25 20:48:26 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-var rellax = new Rellax('.rellax');
+// var rellax = new Rellax(".rellax");
+
+// document.getElementById("project_1").onclick = function() {
+//   this.style.display = "none";
+// };
+
+console.log("test");
+
+document.getElementByClass("before_they_were_them").onclick = function() {
+  document.getElementByClass("project_1").style.visibility = "hidden";
+};
